7262bd29e474a37364b631af9a08fb5c5dc31c9a
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2022-05-13 14:28:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=eb31f3fd`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=eb31f3fd`;
     const response = await fetch(url);
     const responseJson = await response.json();
 
